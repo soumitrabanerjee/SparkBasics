930926078aa5810bd4103e5231b6e7d4b76275c6
soumitrabanerjee <soumitra.banerjee@hotmail.com>
2024-07-14 23:34:36 +0530

    DataSets added

--- src/main/scala/org/example/SparkBasics.scala
+++ src/main/scala/org/example/SparkBasics.scala
@@ -1,7 +1,15 @@
 package org.example
 
-class SparkBasics extends App{
+import org.apache.spark.sql.SparkSession
+
+object SparkBasics {
   def main(args: Array[String]): Unit={
-    
+    val spark = SparkSession.
+      builder().
+      appName("SparkBasics").
+      getOrCreate()
+    val df = spark.range(100).toDF
+    df.cache.count
+    df.write.format("csv").save("/Users/soumitrabanerjee/Desktop/")
   }
 }
